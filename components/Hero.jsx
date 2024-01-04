8b5bd967c8f2ab2efcd788b96b795c844c963b56
Atharv Vani <atharvavani110@gmail.com>
2024-01-04 10:15:25 +0530

    Redux demo added

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState, useEffect } from "react";
 import Avatar from "@/public/data/images/avatar.jpg";
 import AvatarVector from "@/public/data/images/avatar_vector.png";
 import { useSelector } from "react-redux";
@@ -7,20 +8,76 @@ import Link from "next/link";
 
 import { SkillSet, SectionBtn } from "./Export";
 
+import { motion } from "framer-motion";
+
+import { Tooltip } from "@nextui-org/react";
+
 import { FaGithub, FaLinkedin, FaTwitter } from "./IconsExport";
 
 const Hero = () => {
   const state = useSelector((state) => state.changeAvatar.flag);
+  const [isOpen, setIsOpen] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsOpen(false);
+    }, 1000);
+  }, []);
+
   return (
     <section className="flex flex-col gap-4">
-        <Image
-          src={state ? Avatar : AvatarVector}
-          width={200}
-          height={200}
-          alt="avatar"
-          className="w-[150px] avatar rounded-3xl"
-          unoptimized
-        />
+      <Tooltip
+        showArrow
+        placement="right"
+        isDisabled={state}
+        content={
+          <div>
+            <h1 className="font-medium">Magic🔮</h1>
+            <p className="text-xs">
+              Click on <span className="font-medium">Navbar</span>{" "}
+              Image/Avatar
+            </p>
+          </div>
+        }
+        classNames={{
+          content: ["py-2 px-4 rounded-lg"],
+        }}
+      >
+        <div className="w-fit flex items-center gap-3">
+          <Image
+            src={state ? Avatar : AvatarVector}
+            width={200}
+            height={200}
+            alt="avatar"
+            className="w-[150px] avatar rounded-3xl"
+            unoptimized
+          />
+          {state ? (
+            <motion.div
+              initial={{ opacity: 0, scale: 0 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.4,
+                delay: 0.2,
+                ease: [0.25, 0.5, 0.75, 1],
+              }}
+            >
+              <p className="py-2 px-4 flex items-center justify-start tracking-normal border bg-white text-sm font-medium rounded-lg shadow-md">
+                Redux&nbsp;
+                <Image
+                  src="https://skillicons.dev/icons?i=redux&theme=dark"
+                  width={20}
+                  height={20}
+                  alt="redux logo"
+                />
+                &nbsp;Magic🔮
+              </p>
+            </motion.div>
+          ) : (
+            <></>
+          )}
+        </div>
+      </Tooltip>
       <div className="flex flex-wrap items-end text-xl gap-4">
         <h1 className="text-3xl font-bold leading-none">Atharv Vani</h1>
         <div className="flex items-center gap-2">
